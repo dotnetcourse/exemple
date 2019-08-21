c7713e9500cfc8fd34fb7286f066bd41c5092807
Radu Pascal <rpascal@sdl.com>
2019-08-21 19:17:48 +0300

    Adaugat lucru cu fisiere

---
+++ LucruCuFisiere/ConsoleApp21/Carte.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ConsoleApp21
+{
+	public class Carte
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Camp obligatoriu")]
+		[StringLength(50, ErrorMessage = "Numele Cartii trebuie sa fie intre 2 si 50 caracter", MinimumLength = 2)]
+		[Display(Name = "Numele Cartii")]
+		public string NumeleCartii { get; set; }
+
+		[Required]
+		[Display(Name = "Autor")]
+		public string NumeAutor { get; set; }
+
+		//[Display(Name = "Autor")]
+		//public int AutorId { get; set; }
+
+		[Display(Name = "Editura")]
+		public string Editura { get; set; }
+
+		[Display(Name = "Data Publicarii")]
+		[DataType(DataType.Date)]
+		public DateTime DataPublicarii { get; set; }
+
+		[Range(20, 20000)]
+		[Display(Name = "Numar Pagini")]
+		public int NumarPagini { get; set; }
+
+		[StringLength(50, ErrorMessage = "Introduceti un Abstract de minim 50 de caractere")]
+		//[DataType(DataType)]
+		public string Abstract { get; set; }
+	}
+}
---
+++ LucruCuFisiere/ConsoleApp21/Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleApp21
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//FileStream myFile = File.Open("c:\\temp1\\testfisier.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+			//StreamWriter myWriter = new StreamWriter(myFile);
+			//myWriter.Write("Maria are doua mer.");
+			//myWriter.WriteLine("Vlad are trei pere.");
+			//myWriter.Flush();
+
+			//string textDinFisier = File.ReadAllText("c:\\temp1\\testfisier.txt");
+			//Console.WriteLine(textDinFisier);
+
+			//FileStream myFile = File.Open("c:\\temp1\\testfisier.txt", FileMode.Open, FileAccess.ReadWrite);
+			//StreamReader myReader = new StreamReader(myFile);
+
+			////while (!myReader.EndOfStream)
+			////{
+			////	string linie = myReader.ReadLine();
+
+			////	Console.WriteLine(linie);
+			////}
+
+			//string totTextul = myReader.ReadToEnd();
+
+			//myReader.Close();
+			//myFile.Close();
+
+			//foreach (string folder in Directory.EnumerateDirectories("c:\\users\\"))
+			//	{
+			//	Console.WriteLine(folder);
+			//}
+
+			//string basePath = "c:\\temp1";
+			//string userPath = "rpascal";
+			//string file = "somefile.txt";
+
+			//string finalPath = Path.Combine(Path.Combine(basePath, userPath), file);
+
+			//Carte carte = new Carte
+			//{
+			//	Abstract = "detalii despre carte",
+			//	DataPublicarii = DateTime.Now,
+			//	Id = 1,
+			//	NumeAutor = "Jack London",
+			//	NumeleCartii = "White Fang"
+			//};
+
+
+
+			//string serializedObject = JsonConvert.SerializeObject(carte);
+			//File.WriteAllText("c:\\temp1\\carte.json", serializedObject);
+			//Console.WriteLine(serializedObject);
+
+			//string serializedObject = File.ReadAllText("c:\\temp1\\carte.json");
+			//Carte carte = JsonConvert.DeserializeObject<Carte>(serializedObject);
+
+			XmlSerializer serializer = new XmlSerializer(typeof(Carte));
+
+			FileStream myFile = File.Open("c:\\temp1\\xmlfile.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+			//serializer.Serialize(myFile, carte);
+			//myFile.Flush();
+			//myFile.Close();
+
+			Carte carte = (Carte)serializer.Deserialize(myFile);
+			myFile.Close();
+
+			Console.ReadKey();
+
+		}
+	}
+}
